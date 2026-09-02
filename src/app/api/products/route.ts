7e0af9f2b6b87b8118595538c7575a178c2f8590
Mohamed Ramadan <mohamedramadansaif1122@gmail.com>
2026-09-02 17:28:33 +0300

    Reorganize project: move all files into ecobazar_Frontend and ecobazar_Backend subdirectories

--- src/app/api/products/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getAllProducts } from "@/api/products/products.api";
-
-export async function GET() {
-    const res = await getAllProducts();
-    return NextResponse.json(res)
-}
